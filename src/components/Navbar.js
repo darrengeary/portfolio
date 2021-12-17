8228f796c9b83e787fc81d2ca6ed1e74ef8b3c1a
Darren <gearydarren@gmail.com>
2021-12-17 17:17:57 +0000

    Build

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,17 +12,17 @@ export default function Navbar() {
             Darren Geary
         </a>
         </div>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#experience" className="mr-5 hover:text-white">
-            Past Work
-          </a>
-          <a href="#skills" className="mr-5 hover:text-white">
-            Skills
-          </a>
-          <a href="#educations" className="mr-5 hover:text-white">
-            Education
-          </a>
-        </nav>
+          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+            <a href="#experience" className="mr-5 hover:text-white">
+              Past Work
+            </a>
+            <a href="#skills" className="mr-5 hover:text-white">
+              Skills
+            </a>
+            <a href="#educations" className="mr-5 hover:text-white">
+              Education
+            </a>
+          </nav>
         <a
           href="https://www.linkedin.com/in/darren-geary/"
           className="inline-flex items-center bg-gray-800 border-0 py-1 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
